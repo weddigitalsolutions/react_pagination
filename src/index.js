a67f3b4d31e344f8eaeae17b64963bfbcad17546
wspfilho <weddigitalsolutions@users.noreply.github.com>
2020-06-01 16:24:59 -0300

    setted up bootstrap for the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//BOOTSTRAP AND STYLES --- BEGIN
+import "bootstrap/dist/css/bootstrap.min.css";
+// eslint-disable-next-line
+import $ from "jquery";
+// eslint-disable-next-line
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
